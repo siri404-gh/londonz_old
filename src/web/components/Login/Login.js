230b34eacd25150756fe397b755e971bf9741338
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-11-25 17:44:31 +0000

    fix: fix to loading indicator

--- src/web/components/Login/Login.js
+++ src/web/components/Login/Login.js
@@ -21,22 +21,20 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-
+import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
 // Styles
 import styles from './login.css'; // This uses CSS modules.
 import './firebaseui-styling.global.css'; // Import globally.
 const { navbar: { title }, firebaseConfig } = require('../../../../config/variables');
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
+const delay = 2000;
 class Login extends React.Component {
   uiConfig = {
     signInFlow: 'redirect',
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      // firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
       signInSuccessWithAuthResult: () => false,
@@ -50,7 +48,7 @@ class Login extends React.Component {
   componentDidMount() {
     this.unregisterAuthObserver = firebaseApp.auth().onAuthStateChanged((user) => {
       this.setState({ isSignedIn: !!user });
-      if(user) this.props.history.push('/app');
+      if(user) setTimeout(() => this.props.history.push('/app'), delay);
     });
   }
 
@@ -59,13 +57,15 @@ class Login extends React.Component {
   }
 
   render() {
+    console.log(this.state.isSignedIn);
     return (
       <div className="container">
         <div className="logo">
           <h1 className="caption">{title}</h1>
           <img className="logoIcon" src="/img/logo-192.png"/>
         </div>
-        {!this.state.isSignedIn && <StyledFirebaseAuth className={styles.firebaseUi} uiConfig={this.uiConfig} firebaseAuth={firebaseApp.auth()}/>}
+        {this.state.isSignedIn !== undefined && !this.state.isSignedIn && <StyledFirebaseAuth className={styles.firebaseUi} uiConfig={this.uiConfig} firebaseAuth={firebaseApp.auth()}/>}
+        {(this.state.isSignedIn === undefined || this.state.isSignedIn) && <LoadingIndicator />}
       </div>
     );
   }
--- src/web/routes.js
+++ src/web/routes.js
@@ -1,8 +1,8 @@
 import { MemoryRouter as Router, Route, Switch } from 'react-router-dom';
 import React, { Suspense, lazy } from 'react';
 import LinearIndeterminate from './components/LinearIndeterminate/LinearIndeterminate';
-const App = lazy(() => import('./components/App/App'));
-// import App from './components/App/App';
+// const App = lazy(() => import('./components/App/App'));
+import App from './components/App/App';
 const Login = lazy(() => import('./components/Login/Login'));
 
 const { login } = require('../../config/variables');
